2d4703ac6c5f0a32f6bc49edad028c058adb61a8
Krzysztof G <krz.gonia@gmail.com>
2023-04-20 23:56:47 +0200

    clip interrogator

--- models/image_text_transformation.py
+++ models/image_text_transformation.py
@@ -1,4 +1,5 @@
 from models.blip2_model import ImageCaptioning
+from models.interrogator import ClipInterrogator
 from models.grit_model import DenseCaptioning
 from models.gpt_model import ImageToText
 from models.controlnet_model import TextToImage
@@ -30,7 +31,7 @@ class ImageTextTransformation:
         print('\033[1;34m' + "Welcome to the Image2Paragraph toolbox...".center(50, '-') + '\033[0m')
         print('\033[1;33m' + "Initializing models...".center(50, '-') + '\033[0m')
         print('\033[1;31m' + "This is time-consuming, please wait...".center(50, '-') + '\033[0m')
-        self.image_caption_model = ImageCaptioning(device=self.args.image_caption_device, captioner_base_model=self.args.captioner_base_model)
+        self.image_caption_model = ClipInterrogator(device=self.args.image_caption_device)
         self.dense_caption_model = DenseCaptioning(device=self.args.dense_caption_device)
         self.gpt_model = ImageToText(openai_key)
         self.controlnet_model = TextToImage(device=self.args.contolnet_device)
---
+++ models/interrogator.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from clip_interrogator import Interrogator, Config
+
+
+class ClipInterrogator:
+    def __init__(self, device, captioner_base_model='blip'):
+        self.device = device
+        self.ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
+
+    def image_caption(self, image_src):
+        image = Image.open(image_src).convert('RGB')
+        generated_text = self.ci.interrogate(image)
+        print('\033[1;35m' + '*' * 100 + '\033[0m')
+        print('\nStep1, Clip Interrogator caption:')
+        print(generated_text)
+        print('\033[1;35m' + '*' * 100 + '\033[0m')
+        return generated_text
+
+    def image_caption_debug(self, image_src):
+        return "A dish with salmon, in style of Helmut."
